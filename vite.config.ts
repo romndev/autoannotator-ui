544a40095e947ef8a536f741ded3f5f349facf7b
roman.savoskin <roman.savoskin@ailet.com>
2023-09-08 23:54:05 +0400

    Adding support of environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import 'dotenv/config'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     strictPort: true,
-    port: 8080,
+    port: process.env.PORT ?? 8080,
     proxy: {
       '^/api.*': {
-        target: 'http://aa_client:8000',
+        target: process.env.API_HOST ?? 'http://localhost:8000',
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     }
